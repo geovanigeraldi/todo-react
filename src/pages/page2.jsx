4852cbd78de2ed7e4c03928981beebbe34b7ae88
Geovani Geraldi <geovani.geraldi@gmail.com>
2021-04-24 09:47:18 -0300

    adicionar

--- src/pages/page2.jsx
+++ src/pages/page2.jsx
@@ -1,26 +1,54 @@
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid'
 
-function Page2(){
+function Page2() {
+  const [titulo, setTitulo] = useState("");
+  const [titulos, setTitulos] = useState([]);
 
+  function handleAdicionar() {
+    if (titulo) {
+
+      const arrTitulos = {
+        id: uuid(),
+        titulo: titulo
+      }
+
+      const arrNew = [...titulos];
+      arrNew.unshift(arrTitulos);
+
+      setTitulos(arrNew);
+      setTitulo("");
+      //console.log(arrNew);           
+    }
+  }
 
   return (
     <div>
+      
       <div>
-        <input 
-          type="text" 
-          
+        <input
+          type="text"
+          onChange={((e) => setTitulo(e.target.value))}
+          value={titulo}
         />
         <button
-          
+          type="button"
+          onClick={handleAdicionar}
         >
           Adicionar
         </button>
       </div>
-      
+
       <div>
-        <div>
-          titulo
-        </div>
-        <button>Excluir</button>
+        {titulos.map((titulo) => (
+          <div key={titulo.id}>
+            <p>
+              {titulo.titulo}
+            </p>
+            <button>Excluir</button>
+          </div>
+        ))
+        }
       </div>
       
     </div>
